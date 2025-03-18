b2c69a0709fb42204819bed05338fc61c3300c96
A00488698 <Yaxuan.Zhang@smu.ca>
2025-03-18 18:16:34 -0300

    remove test file

--- backend/test.py
+++
@@ -1,8 +0,0 @@
-import os
-import google.generativeai as genai
-from dotenv import load_dotenv
-from pathlib import Path
-
-genai.configure(api_key="AIzaSyBZJJNl9yAycmVc141ZUc0YHqS84eVoAlc")
-print(genai.GenerativeModel('gemini-1.5-pro').generate_content("你好").text)
-
